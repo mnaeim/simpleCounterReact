d7cbf5c45ccef76ff1152b921fa051091b243e45
Mazen Naeim <naeimmazen@gmail.com>
2021-12-06 03:22:41 +0000

    done

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-//create your first component
+import { useState, useEffect } from "react";
+
 const SecondsCounter = () => {
+	const [count, setCount] = useState(0);
+
+	setTimeout(() => {
+		setCount(count => count + 1);
+	}, 1000);
+
+	const number = Math.floor(count / 1000);
+	const one = Math.floor(count / 100);
+	const two = Math.floor(count / 10);
+	const three = Math.floor(count / 1);
+
 	return (
-		<div classname="bigCounter">
+		<div className="bigCounter">
 			<div className="calender">
 				<i className="far fa-clock"></i>
 			</div>
-			<div className="four">0</div>
-			<div className="three">0</div>
-			<div className="two">0</div>
-			<div className="one">0</div>
+			<div className="four">{number % 10}</div>
+			<div className="three">{one % 10}</div>
+			<div className="two">{two % 10}</div>
+			<div className="one">{three % 10}</div>
 		</div>
 	);
 };
